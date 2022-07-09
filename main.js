624cd1ee1e62229ee796ef7b16b59c415909107d
oh <outputor777@gmail.com>
2022-07-09 22:22:55 +0900

    add paintJS

---
+++ main.js
@@ -0,0 +1,43 @@
+const canvas = document.getElementById('jsCanvas');
+const color = document.querySelectorAll('.color');
+const ctx = canvas.getContext('2d');
+
+canvas.width = 900;
+canvas.height = 850;
+ctx.lineWidth = 1;
+ctx.strokeStyle = '#212529';
+let stroke = false;
+
+function mouseMoveHandler(event) {
+  let x = event.offsetX;
+  let y = event.offsetY;
+  if (!stroke) {
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+  } else {
+    ctx.lineTo(x, y);
+    ctx.stroke();
+  }
+}
+
+function colorChangeHandler(event) {
+  ctx.strokeStyle = event.target.style.backgroundColor;
+}
+
+function mouseDownHandler() {
+  stroke = true;
+}
+
+function stopHandler() {
+  stroke = false;
+}
+
+if (canvas) {
+  canvas.addEventListener('mousemove', mouseMoveHandler);
+  canvas.addEventListener('mousedown', mouseDownHandler);
+  canvas.addEventListener('mouseleave', stopHandler);
+  canvas.addEventListener('mouseup', stopHandler);
+  color.forEach((el) => {
+    el.addEventListener('click', colorChangeHandler);
+  });
+}
